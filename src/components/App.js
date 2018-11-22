c03cbea87743de679e0fd8cf27c64ec219dc155c
mythLabs <amitbisht744@gmail.com>
2018-11-22 10:59:04 +0530

    base setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    render() {
+        return ( 
+            <div>
+            <SearchBar />
+             <div>App</div>
+             </div>
+            )
+    }
+}
+
+export default App;
